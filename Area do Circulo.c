71651cb1bd191cad6d325379da9d4590941263e4
Thales Fortes <thales.fortes21@gmail.com>
2021-08-26 08:35:33 -0300

    Create Area do Circulo.c

---
+++ Area do Circulo.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+ 
+    double A, raio;
+    scanf("%lf", raio);
+    A = 3.14159 * raio*raio;
+    printf("A = %.4lf\n", A);
+
+    return 0;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
