d0c9a73c54167a980bf7a473cef18c71089c82a4
Thales Fortes <thales.fortes21@gmail.com>
2021-08-28 11:57:31 -0300

    Create diferenca.c

---
+++ diferenca.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+ 
+    int A, B, C, D, x;
+    scanf("%d%d%d%d", &A, &B, &C, &D);
+    x = (A * B) - (C * D);
+    printf("DIFERENCA = %d\n", x );
+
+    return 0;
+    
+}
